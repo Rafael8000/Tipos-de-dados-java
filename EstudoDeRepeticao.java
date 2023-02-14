838296b16902b818872d8aa1b30d5bea684f843d
Rafael8000 <rafael.pomari@etec.sp.gov.br>
2023-02-14 14:14:08 +0000

    maconha

--- EstudoDeRepeticao.java
+++ EstudoDeRepeticao.java
@@ -3,8 +3,8 @@ public class EstudoDeRepeticao{
     public static void main(String[] args){
 
         int cc = 0;
-        while(cc<4){
-            System.out.println("teste" + (cc+1));
+        while(cc<1000){
+            System.out.println( + (cc+1));
         cc++;
         }
     
