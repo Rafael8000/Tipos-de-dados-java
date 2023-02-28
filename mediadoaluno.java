f857836a0c224d3710aa072c3ab42e93036bb556
Rafael8000 <rafael.pomari@etec.sp.gov.br>
2023-02-28 14:06:20 +0000

    salsicha

--- mediadoaluno.java
+++ mediadoaluno.java
@@ -14,12 +14,23 @@ public static void main(String[] args){
 
     media = (n1 + n2)  /2;
 
-    System.out.printf("Sua primeira nota foi =" + n1);
-    System.out.printf(" \n Sua segunda nota foi =" + n2);
+    System.out.printf("Sua primeira nota foi = " + n1);
+    System.out.printf(" \n Sua segunda nota foi = " + n2);
     System.out.printf(" \n Sua média é foi = " + media + "\n");
+if (media >= 8 && media <= 10)
+{
+    System.out.print("Aluno está aprovado \n");
 
-
-
+}
+else if (media >= 5 && media <= 7) {
+    System.out.print("Aprovado, mas terá que fazer recuperação \n");
+}
+else if (media >= 0 && media <= 4){
+    System.out.print("Aluno está reprovado \n" );
+}
+else{
+    System.out.print("erro na media \n");
+}
 
 }
 
